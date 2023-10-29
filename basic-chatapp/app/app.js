53b77526ee3645a8b2cda3a0afb352b7e2d2bbc8
rahul <rahulrao7144@gmail.com>
2023-10-30 01:02:24 +0530

    Authentication and Room specific emits

--- basic-chatapp/app/app.js
+++ basic-chatapp/app/app.js
@@ -1,4 +1,8 @@
-const socket = io('ws://localhost:8080');
+const socket = io('ws://localhost:8080', {
+	auth: {
+		token: 'invalid-token',
+	},
+});
 
 socket.on('message', (text) => {
     const el = document.createElement('li');
@@ -6,7 +10,16 @@ socket.on('message', (text) => {
     document.querySelector('ul').appendChild(el);
 });
 
+/* When auth token is invalid, update token and try to connect again */
+socket.on('disconnect', () => {
+	console.log("DISCONNECTED");
+	socket.auth.token = 'updated-token';
+	socket.connect();
+})
+
 document.querySelector('button').onclick = () => {
     const text = document.querySelector('input').value;
-    socket.emit('message', text);
+    socket.emit('message', {socket_id: socket.id, text}, (response) => {
+        console.log("SERVER -> CLIENT", {response});
+    });
 };
--- basic-chatapp/server/index.js
+++ basic-chatapp/server/index.js
@@ -1,17 +1,33 @@
-const http = require('http').createServer();
+const http = require ('http').createServer ();
 
-const io = require('socket.io')(http, {
-    cors: { "origin": "*" }
+const io = require ('socket.io') (http, {
+  cors: {origin: '*'},
 });
 
-io.on('connection', (socket) => {
-    console.log('Connected to: ', socket.id);
+const connections = [];
 
-    socket.on('message', (message) => {
-        io.emit('message', `${socket.id}: ${message}`);
-    });
+io.on ('connection', socket => {
+  console.log ('Connected to: ', socket.id);
+	console.log("Auth: ", socket.handshake.auth);
+
+	/* When token is invalid, disconnect socket */
+	if (socket.handshake.auth.token === 'invalid-token') {
+		console.log("TOKEN ERROR");
+		socket.disconnect(true);
+	}
+	/* Add first 5 connections to room_1 and rest of the connections to room_2 */
+	connections.push(socket.id);
+	const roomId = connections.length < 5 ? 'room_1' : 'room_2';
+	socket.join(roomId);
+
+	/* Emit room specific messages */
+  socket.on ('message', (message, callback) => {
+    io.to('room_1').emit ('message', `${socket.id}: ${message.text}`);
+    console.log ('message', `${socket.id}: ${JSON.stringify (message)}`);
+    callback (`Got Response: ${socket.id}`);
+  });
 });
 
-http.listen(8080, () => {
-    console.log('Listening on port: 8080');
+http.listen (8080, () => {
+  console.log ('Listening on port: 8080');
 });
