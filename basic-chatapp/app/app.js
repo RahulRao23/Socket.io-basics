34fc8eddb0249e3f946eecba03132915dc2b161a
rahul <rahulrao7144@gmail.com>
2023-10-31 00:08:53 +0530

    Formatted documents

--- basic-chatapp/app/app.js
+++ basic-chatapp/app/app.js
@@ -1,25 +1,25 @@
-const socket = io('ws://localhost:8080', {
-	auth: {
-		token: 'invalid-token',
-	},
+const socket = io ('ws://localhost:8080', {
+  auth: {
+    token: 'invalid-token',
+  },
 });
 
-socket.on('message', (text) => {
-    const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el);
+socket.on ('message', text => {
+  const el = document.createElement ('li');
+  el.innerHTML = text;
+  document.querySelector ('ul').appendChild (el);
 });
 
 /* When auth token is invalid, update token and try to connect again */
-socket.on('disconnect', () => {
-	console.log("DISCONNECTED");
-	socket.auth.token = 'updated-token';
-	socket.connect();
-})
+socket.on ('disconnect', () => {
+  console.log ('DISCONNECTED');
+  socket.auth.token = 'updated-token';
+  socket.connect ();
+});
 
-document.querySelector('button').onclick = () => {
-    const text = document.querySelector('input').value;
-    socket.emit('message', {socket_id: socket.id, text}, (response) => {
-        console.log("SERVER -> CLIENT", {response});
-    });
+document.querySelector ('button').onclick = () => {
+  const text = document.querySelector ('input').value;
+  socket.emit ('message', {socket_id: socket.id, text}, response => {
+    console.log ('SERVER -> CLIENT', {response});
+  });
 };
--- basic-chatapp/server/index.js
+++ basic-chatapp/server/index.js
@@ -2,27 +2,31 @@ const http = require ('http').createServer ();
 
 const io = require ('socket.io') (http, {
   cors: {origin: '*'},
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 2 * 60 * 1000,
+    skipMiddlewares: true,
+  },
 });
 
 const connections = [];
 
 io.on ('connection', socket => {
   console.log ('Connected to: ', socket.id);
-	console.log("Auth: ", socket.handshake.auth);
+  console.log ('Auth: ', socket.handshake.auth);
 
-	/* When token is invalid, disconnect socket */
-	if (socket.handshake.auth.token === 'invalid-token') {
-		console.log("TOKEN ERROR");
-		socket.disconnect(true);
-	}
-	/* Add first 5 connections to room_1 and rest of the connections to room_2 */
-	connections.push(socket.id);
-	const roomId = connections.length < 5 ? 'room_1' : 'room_2';
-	socket.join(roomId);
+  /* When token is invalid, disconnect socket */
+  if (socket.handshake.auth.token === 'invalid-token') {
+    console.log ('TOKEN ERROR');
+    socket.disconnect (true);
+  }
+  /* Add first 5 connections to room_1 and rest of the connections to room_2 */
+  connections.push (socket.id);
+  const roomId = connections.length < 5 ? 'room_1' : 'room_2';
+  socket.join (roomId);
 
-	/* Emit room specific messages */
+  /* Emit room specific messages */
   socket.on ('message', (message, callback) => {
-    io.to('room_1').emit ('message', `${socket.id}: ${message.text}`);
+    io.to ('room_1').emit ('message', `${socket.id}: ${message.text}`);
     console.log ('message', `${socket.id}: ${JSON.stringify (message)}`);
     callback (`Got Response: ${socket.id}`);
   });
